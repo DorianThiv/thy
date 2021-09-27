9e8af91345cf640aaea26de40fc918caaed84e2c
Dorian Th <dorianth@orange.fr>
2021-09-27 18:36:13 +0200

    Update field import

--- thy-file-import/thy-file-import.component.ts
+++ thy-file-import/thy-file-import.component.ts
@@ -4,17 +4,19 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { ThyFileImportService } from './thy-file-import.service';
 import { ThyDialogsService } from '../thy-dialogs/thy-dialogs.service';
 import { ThyTranslateService } from '../thy-translate';
+import { ThyImage } from './thy-image.class';
 
 @Component({
   selector: 'thy-file-import',
   templateUrl: './thy-file-import.component.html',
-  styleUrls: ['../thy-styles/thy-style.scss', './thy-file-import.component.scss']
+  styleUrls: ['./thy-file-import.component.scss']
 })
 export class ThyFileImportComponent {
 
   public file: File;
   public fileSrc: SafeUrl;
 
+  private image: ThyImage;
   private _urlCreator = window.URL;
 
   public loading = false;
@@ -28,16 +30,7 @@ export class ThyFileImportComponent {
   }
 
   public async validate() {
-    this.loading = true;
-    const response = await this.service.upload(this.file);
-    this.loading = false;
-    if (response) {
-      this.dialogRef.close(this.file);
-    } else {
-      this.dialogsService.info({ title: this.translateService.instant('@global-warning'), message: 'Cannot import file.' }).subscribe(() => {
-        this.dialogRef.close();
-      });
-    }
+    this.dialogRef.close(this.image);
   }
 
   public close() {
@@ -45,7 +38,13 @@ export class ThyFileImportComponent {
   }
 
   public async onFileChange(files: FileList) {
-    this.file = files ? files.item(0) : null;
+    const file = files ? files.item(0) : null;
+    this.file = file;
+    let reader = new FileReader();
+    reader.readAsDataURL(this.file);
+    reader.onload = async () => {
+      this.image = new ThyImage(file.name, file.type, (reader.result as string).split(',')[1]);
+    }
     this.fileSrc = this.sanitizer.bypassSecurityTrustUrl(this._urlCreator.createObjectURL(this.file));
   }
 
---
+++ thy-file-import/thy-image.class.ts
@@ -0,0 +1,13 @@
+export class ThyImage {
+
+    public name: string;
+    public type: string;
+    public content: string;
+    
+    constructor(name: string, type: string, content: string) {
+        this.name = name; 
+        this.type = type;
+        this.content = content;
+    }
+
+}
